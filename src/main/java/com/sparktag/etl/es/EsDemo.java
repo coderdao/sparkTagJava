c28cf752ed4179bfd55b6f77a1ce85638318fbf8
abo <abo2013@foxmail.com>
2021-08-06 17:44:15 +0800

    up

--- src/main/java/com/sparktag/etl/es/EsDemo.java
+++ src/main/java/com/sparktag/etl/es/EsDemo.java
@@ -36,10 +36,5 @@ public class EsDemo {
     public static class User implements Serializable {
         private String name;
         private Integer age;
-
-        public User(String name, Integer age) {
-            this.name = name;
-            this.age = age;
-        }
     }
 }
