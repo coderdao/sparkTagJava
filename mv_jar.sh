72f8c52e4c95ed961c786ab742fdd89df89b044e
abo <abo2013@foxmail.com>
2021-08-06 11:38:48 +0800

    update

---
+++ mv_jar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# encoding: utf-8.0
+
+mvn clean
+mvn compile
+mvn package
+
+rm ~/Downloads/spark_tag-1.0-SNAPSHOT.jar
+mv ./target/spark_tag-1.0-SNAPSHOT.jar ~/Downloads/spark_tag-1.0-SNAPSHOT.jar
