530126672ac9348cb4308b3050eb199aaaa68f1b
abo <abo2013@foxmail.com>
2021-08-07 10:06:27 +0800

    up

--- src/main/java/com/sparktag/etl/es/EsDemo.java
+++ src/main/java/com/sparktag/etl/es/EsDemo.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.StreamingContext;
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
@@ -18,17 +21,17 @@ public class EsDemo {
         SparkConf conf = new SparkConf()
                 .setMaster("local[*]")
                 .setAppName("es_demo")
-                .set(ConfigurationOptions.ES_NET_HTTP_AUTH_USER, "password")
-                .set(ConfigurationOptions.ES_NET_HTTP_AUTH_PASS, "password")
-                .set("es.nodes", "namenode")
+                .set("es.nodes", "192.168.20.157")
+                .set("es.port", "9200")
+                .set("es.nodes.wan.only", "true")
                 .set("es.index.auto.create", "true"); // 自动创建不存在 index 索引
-
-        JavaSparkContext jsc = new JavaSparkContext(conf);
 
         List<User> list = new ArrayList<>();
         list.add(new User("Jack", 18));
         list.add(new User("Eric", 28));
 
+        // hadoop - es
+        JavaSparkContext jsc = new JavaSparkContext(conf);
         JavaRDD<User> userJavaRDD = jsc.parallelize(list);
         JavaEsSpark.saveToEs(userJavaRDD, "/user/_doc");
     }
