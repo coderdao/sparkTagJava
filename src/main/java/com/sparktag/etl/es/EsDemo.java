cedab65e41a77f1a2d8912e5c70536ce42ec9a5b
abo <abo2013@foxmail.com>
2021-08-07 11:06:26 +0800

    [A[B查询方式

--- src/main/java/com/sparktag/etl/es/EsDemo.java
+++ src/main/java/com/sparktag/etl/es/EsDemo.java
@@ -3,18 +3,23 @@ package com.sparktag.etl.es;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.StreamingContext;
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+import scala.Tuple2;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class EsDemo {
     public static void main(String[] arge) {
@@ -26,14 +31,36 @@ public class EsDemo {
                 .set("es.nodes.wan.only", "true")
                 .set("es.index.auto.create", "true"); // 自动创建不存在 index 索引
 
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        /** ============================== 插入数据 ============================== */
+        /*
         List<User> list = new ArrayList<>();
         list.add(new User("Jack", 18));
         list.add(new User("Eric", 28));
 
         // hadoop - es
-        JavaSparkContext jsc = new JavaSparkContext(conf);
         JavaRDD<User> userJavaRDD = jsc.parallelize(list);
         JavaEsSpark.saveToEs(userJavaRDD, "/user/_doc");
+        */
+
+        JavaPairRDD<String, Map<String, Object>> pairRDD = JavaEsSpark.esRDD(jsc, "/user/_doc");
+        Map<String, Map<String, Object>> stringMapMap = pairRDD.collectAsMap();
+        System.out.println("原始数据 ============ "+stringMapMap);
+
+        // pairRDD 转 user
+        JavaRDD<User> rdd = pairRDD.map(new Function<Tuple2<String, Map<String, Object>>, User>() {
+            @Override
+            public User call(Tuple2<String, Map<String, Object>> v1) throws Exception {
+                User user = new User();
+                BeanUtils.populate(user, v1._2());
+
+                return user;
+            }
+        });
+
+        List<User> collect = rdd.collect();
+        System.out.println("原始数据 ============ "+collect);
     }
 
     @Data
